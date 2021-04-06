98ecd8e8d1ba91cda0380e7d319a5ed7cbe7aff1
Zack <zack871024@gmail.com>
2021-04-05 20:07:38 -0700

    Finished project 1 page
    Finished project 1 page

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,52 +1,64 @@
 /*CITATION */
 /*https://www.youtube.com/watch?v=inCl01EJkts*/
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function() {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+  $(".menu-toggler").on("click", function () {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
 
-    $('.top-nav .nav-link').on('click', function() {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
 
-    $('nav a[href*="#"]').on('click', function() {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top-100},2000);
-    });
+  $('nav a[href*="#"]').on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top - 100,
+      },
+      2000
+    );
+  });
 
-    $('#up').on('click', function() {
-        $('html, body').animate({
-            scrollTop: 0},2000);
-    });
+  $("#up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      2000
+    );
+  });
 
-    AOS.init({
-        easing:"ease", 
-        duration: 1800,
-        once: true,
-    });
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true,
+  });
 });
 
 /*https://www.w3schools.com/howto/howto_js_slideshow.asp*/
 
-var slideIndex = [1,1];
+var slideIndex = [1, 1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
+var slideId = ["mySlides1", "mySlides2"];
 showSlides(1, 0);
 showSlides(1, 1);
 
 function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+  showSlides((slideIndex[no] += n), no);
 }
 
 function showSlides(n, no) {
   var i;
   var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no] - 1].style.display = "block";
 }
